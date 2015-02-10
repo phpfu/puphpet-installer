f5a18283fbe5b1d970be61d3b88604af0b3c045a
Brian Porter <beporter@users.sourceforge.net>
2015-02-10 12:57:45 -0600

    Experimenting with installing to the project root.
    Now seems "minimally successful."

--- src/Loadsys/Composer/PuphpetReleaseInstallerPlugin.php
+++ src/Loadsys/Composer/PuphpetReleaseInstallerPlugin.php
@@ -2,12 +2,26 @@
 
 namespace Loadsys\Composer;
 
+// Needed for PluginInterface:
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
+/**
+ * Plugin entry point.
+ *
+ */
 class PuphpetReleaseInstallerPlugin implements PluginInterface {
+
+    /**
+	 * Activate the plugin (called from {@see \Composer\Plugin\PluginManager})
+	 *
+	 * @param \Composer\Composer $composer The active instance of the composer base class.
+	 * @param \Composer\IO\IOInterface $io The I/O instance.
+     * @return void
+     */
     public function activate(Composer $composer, IOInterface $io) {
+        // Register the custom installer.
         $installer = new PuphpetReleaseInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
